42456050d52b9bbff689c033d0894c6a3c10e8f9
soaresgabe <gabriel@daedaluslabs.com.br>
2024-03-15 11:06:00 -0300

    gopherlings

--- 3.longest-substring-without-repeating-characters.go
+++ 3.longest-substring-without-repeating-characters.go
@@ -1,5 +1,10 @@
 // @leet start
 func lengthOfLongestSubstring(s string) int {
-    
+  var substring = make([]string, 0, len(s));
+  var max int = 0;
+  
+  for _, char := range s {
+    if(
+  }
 }
 // @leet end
